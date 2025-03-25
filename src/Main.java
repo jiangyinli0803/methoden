278ee1b290122f44c1f96a1bd2d6a8c20d9c36cd
Yinli <jiangyinli0803@gmail.com>
2025-03-25 14:51:11 +0100

    add Main Class

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bitte deinen Name geben: ");
+        String name = scanner.nextLine();
+        greet(name);
+        String text;
+
+        int sum = sum(2, 6);
+        int substract = substract(2, 6);
+        System.out.println("The Sum is " + sum);
+        System.out.println("The Substract is " + substract);
+
+        double sumDouble = sum(2.5, 3.6);
+        System.out.println("The Sum of 2 kommazahlen is " + sumDouble);
+
+        double makePositive = makePositive(-2.5555);
+        System.out.println("Die positive ist  " + makePositive);
+
+        System.out.println("write a word or sentence: ");
+       text = scanner.nextLine();
+       if(isPalindrome(text)){
+           System.out.println("The word is palindrome.");
+       }else {
+           System.out.println("The word is not palindrome.");
+       }
+
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+    public static int substract(int a, int b) {
+        return a - b;
+    }
+    public static double makePositive(double x) {
+        if (x < 0) {
+            return -x;
+        }else{
+            return x;
+        }
+    }
+
+    public static boolean isPalindrome(String text) {
+        text = text. replaceAll("\\s", "").toLowerCase();
+
+        int left = 0, right = text.length() - 1;
+        while (left < right){
+            if(text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+                left++; right--;
+            }
+        return true;
+    }
+}
